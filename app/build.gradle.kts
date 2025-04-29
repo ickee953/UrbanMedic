ca37be755b50e6455e7a46e77755ca913eb9f79e
ickee953 <ickee953@gmail.com>
2025-04-29 13:19:16 +0300

    UI layouts and styles, toolbar theme.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
 
     defaultConfig {
         applicationId = "ru.urbanmedic.testapp"
-        minSdk = 28
+        minSdk = 27
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
